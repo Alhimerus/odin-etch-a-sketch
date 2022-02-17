9d1f3be504dce0e146bb9bb2a4094eae346028bd
Alhimerus <Alhimerus@gmail.com>
2022-02-17 22:51:18 +0100

    Add the ability to draw by hovering over the box

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const container = document.getElementById("container");
 const square = document.createElement("div");
 square.classList.add("square");
 
-let length = 16;
+let length = 50;
 for (let i = 0; i < length; i++) {
   container.appendChild(row.cloneNode());
 }
@@ -13,4 +13,10 @@ for (let i = 0; i < length; i++) {
   for (let j = 0; j < length; j++) {
     rowList[i].appendChild(square.cloneNode());
   }
+}
+const squareList = document.getElementsByClassName("square");
+for (let i = 0; i < squareList.length; i++) {
+  squareList[i].addEventListener("pointerover", () => {
+    squareList[i].style.backgroundColor = "blue";
+  })
 }
