43df3aa96553ca6f56b7d67ba5a68cb45ab3e3b2
Alhimerus <Alhimerus@gmail.com>
2022-02-18 15:38:03 +0100

    Let the squares have a random color, user input for number of squares, add button

--- script.js
+++ script.js
@@ -4,19 +4,35 @@ const container = document.getElementById("container");
 const square = document.createElement("div");
 square.classList.add("square");
 
-let length = 50;
-for (let i = 0; i < length; i++) {
-  container.appendChild(row.cloneNode());
-}
-const rowList = document.getElementsByClassName("row");
-for (let i = 0; i < length; i++) {
-  for (let j = 0; j < length; j++) {
-    rowList[i].appendChild(square.cloneNode());
+function createGrid() {
+  let length = parseFloat(prompt("How many squares per side would you like? (MAX 100)"));
+  if (length < 0 || length > 100) {
+    alert("Wrong number!");
+    return createGrid();
+  }
+  for (let i = 0; i < length; i++) {
+    container.appendChild(row.cloneNode());
+  }
+  const rowList = document.getElementsByClassName("row");
+  for (let i = 0; i < length; i++) {
+    for (let j = 0; j < length; j++) {
+      rowList[i].appendChild(square.cloneNode());
+    }
+  }
+  const squareList = document.getElementsByClassName("square");
+  for (let i = 0; i < squareList.length; i++) {
+    let color = randomRGB();
+    squareList[i].addEventListener("pointerover", () => {
+      squareList[i].style.backgroundColor = color;
+    })
   }
 }
-const squareList = document.getElementsByClassName("square");
-for (let i = 0; i < squareList.length; i++) {
-  squareList[i].addEventListener("pointerover", () => {
-    squareList[i].style.backgroundColor = "blue";
-  })
+
+function randomRGB() {
+  let randR = Math.random() * 255;
+  let randG = Math.random() * 255;
+  let randB = Math.random() * 255;
+  return `rgb(${randR}, ${randG}, ${randB})`;
 }
+
+createGrid();
